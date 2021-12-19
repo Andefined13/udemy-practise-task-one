1a19257987f7d9487b52f1ead3706efab10f65e4
Andefined13 <tonyapplgate13@gmail.com>
2021-12-19 20:50:38 +0300

    feat implementing second task

--- app.js
+++ app.js
@@ -6,10 +6,30 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько оцените его?', '');
-      personalMovieDB.movies[a] = b ;
-      personalMovieDB.movies[c] = d ;
-      console.log(personalMovieDB);
+
+      for (let i = 0; i < 2; i++) {
+        const a = prompt("один из последних просмотренных фильмов", ""),
+              b = prompt("на сколько оцените его", "");
+    
+              if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log("done");
+              } else {
+                console.log("error");
+                  i--;
+              }
+    }
+    
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотренно довольно мало фильмов");
+    } else if (10 <= personalMovieDB.count && personalMovieDB.count <= 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+    
+        console.log(personalMovieDB);
+        
+      
